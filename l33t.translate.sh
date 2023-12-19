86733d4aaee62f535f77ca284837c64a49513eda
jamesdank <94645949+jamesdank@users.noreply.github.com>
2023-12-19 17:39:53 -0600

    Update l33t.translate.sh

--- l33t.translate.sh
+++ l33t.translate.sh
@@ -4,5 +4,5 @@ function translate () {
     echo -e "\n\e[4mL33t Speak Converter\e[0m "
     read -p "To Translate: " translate
 
-    echo Translated: "$translate" | tr 'abcdefghijklmnopqrstuvwxyz' '48<d3fg#1jk|m^09q257uvwxyz'
+    echo -n Translated: ; echo " $translate" | tr 'abcdefghijklmnopqrstuvwxyz' '48<d3fg#1jk|m^09q257uvwxyz'
 }
